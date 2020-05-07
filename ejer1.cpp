6f273b7ef1372295bbd58d79e9d3c41269404e50
NhV-117 <000143720@uca.edu.sv>
2020-05-06 18:36:24 -0600

    s commit

---
+++ ejer1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+  int main () {
+
+      int numero;
+
+      cout<<"ingresar numero";
+      cin>>numero;
+
+      cout<<numero;
+
+      return 0;
+ }
