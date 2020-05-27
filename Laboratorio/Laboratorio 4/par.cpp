22e34b667451640a60805b6ca107a653bc165e0a
NhV-117 <62780812+NhV-117@users.noreply.github.com>
2020-05-26 18:56:51 -0600

    1

---
+++ Laboratorio/Laboratorio 4/par.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+main ()
+{
+	int a;
+	
+	cout<<"Ingresar numero: "; cin>>a;
+	
+	if (a%2==0)
+	{
+		cout<<"El numero es par";
+	}else
+	cout<<"El numero es impar";
+	
+	
+	return 0;
+}
