b459ff81601eaf21f50b4ec068bde4a365598904
NhV-117 <62780812+NhV-117@users.noreply.github.com>
2020-06-16 18:57:10 -0600

    version 1.5

--- Laboratorio/Laboratorio 5/dia siguiente.cpp
+++ Laboratorio/Laboratorio 5/dia siguiente.cpp
@@ -10,84 +10,91 @@ main ()
 	cout<<"Ingrese el mes: "; cin>>m;
 	cout<<"Ingrese el año: "; cin>>a;
 	
-	if ((a%400==0)||(a%100!=0)&&(a%4==0)){
-	
+	if ((a%400==0)||(a%100!=0)&&(a%4==0))
+	{
 		if((d>=1)&&(d<=31)&&(m>=1)&&(m<=12))
 		{
 			d++;  // aumento del dia por uno
+			
+			/* Para evitar errores al colocar 31
+			en los meses que tienen 30 dias*/
+			if ((d>31)&&((m==4)||(m==6)||(m==9)||(m==11))){
+				cout<<"\nLa fecha ingresada es invalida";
+			}else
+		{
+			//Evitar errores en el mes de febrero
+			if (d>=31&&m==2){
+				cout<<"\nLa fecha ingresada es invalido";
+			}else{
+			
+			//Dividir los meses que tienen 30 y los que tienen 31
 			if ((d==30)&&(m==2)||(d==31)&&((m==4)||(m==6)||(m==9)||(m==11))||
 			(d==32)&&((m==1)||(m==3)||(m==5)||(m==7)||(m==8)||(m==10)))
 			{
-				d=1;
+				d=1;      //Funcion para cambio de mes
 				m++;
-				
-				cout<<endl;
-			cout<<"El dia siguiente es: \n";
-			cout<<"Dia: " << d << endl;
-			cout<<"Mes: " << m << endl;
-			cout<<"Año: " << a << endl;
 			}
-			if ((d==32)&&(m==12))
+			if ((d==32)&&(m==12)) //Funcion para año nuevo
 			{
 				d=1;
 				m=1;
 				a++;
-				
-			cout<<endl;
-			cout<<"El dia siguiente es: \n";
-			cout<<"Dia: " << d << endl;
-			cout<<"Mes: " << m << endl;
-			cout<<"Año: " << a << endl;
 			}
-			/*En caso de que se ponga un dia mayor a 29
-			en el mes de febrero*/
-			if (d>=31&&m==2)
-			{
-				cout<<"\nLos datos ingresados son invalidos";
-			}	
-		}else
-		{
-			cout<<"\nLos datos ingresados son invalidos";
+		cout<<endl;
+		cout<<"El dia siguiente es: \n";
+		cout<<"Dia: " << d << endl;
+		cout<<"Mes: " << m << endl;
+		cout<<"Año: " << a << endl;	
+			}
 		}
-		
+		}
+			else
+			{
+			cout<<"\nLa fecha ingresada es invalida";
+			}
+	
 	}
 
 	else
 	{
-		d++;  // aumento del dia por uno
-		if ((d==29)&&(m==2)||(d==31)&&((m==4)||(m==6)||(m==9)||(m==11))||
-		(d==32)&&((m==1)||(m==3)||(m==5)||(m==7)||(m==8)||(m==10)))
+		if((d>=1)&&(d<=31)&&(m>=1)&&(m<=12))
 		{
-				d=1;
+			d++;  // aumento del dia por uno
+			
+			/*Evitar errores al colocar 31 en los meses
+			que tienen 30 dias*/
+			if ((d>31)&&((m==4)||(m==6)||(m==9)||(m==11))){
+				cout<<"\nLa fecha ingresada es invalida";
+			}else{
+			if (d>=30&&m==2){ //Errores al colocar dias en febrero
+				cout<<"\nLa fecha ingresada es invalida";
+			}else{
+			
+			//Dividir los meses que tiene 30 dias y los que tienen 31
+			while ((d==29)&&(m==2)||(d==31)&&((m==4)||(m==6)||(m==9)||(m==11))||
+			(d==32)&&((m==1)||(m==3)||(m==5)||(m==7)||(m==8)||(m==10)))
+			{
+				d=1;  //Funcion para cambio de mes
 				m++;
-				
-		cout<<endl;
-		cout<<"El dia siguiente es: \n";
-		cout<<"Dia: " << d << endl;
-		cout<<"Mes: " << m << endl;
-		cout<<"Año: " << a << endl;
-		}
-		if ((d==32)&&(m==12))
-		{
+			}
+			if ((d==32)&&(m==12)) //Funcion para año nuevo
+			{
 				d=1;
 				m=1;
 				a++;
-				
-		cout<<endl;
-		cout<<"El dia siguiente es: \n";
-		cout<<"Dia: " << d << endl;
-		cout<<"Mes: " << m << endl;
-		cout<<"Año: " << a << endl;
+			}
+			cout<<endl;
+			cout<<"El dia siguiente es: \n";
+			cout<<"Dia: " << d << endl;
+			cout<<"Mes: " << m << endl;
+			cout<<"Año: " << a << endl;	
+			}
+			}
 		}
-		/*En caso de que se ponga un dia mayor a 28 en el
-		mes de febrero*/
-		if (d>=30&&m==2)
-		{
-			cout<<"\nLos datos ingresados son invalidos";
-		}else
-		{
-			cout<<"\nLos datos ingresados son invalidos";
-		}
+			else
+			{
+			cout<<"\nLa fecha ingresada es invalida";
+			}
 	}
 	
 	return 0;
